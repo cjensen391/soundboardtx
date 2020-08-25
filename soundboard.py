82e4207c6dcfed351ec5cd30b8fb66f041a8bb43
cjensen391 <cjensen391@gmail.com>
2020-08-25 19:57:12 -0400

    file assignment to buttons.

--- soundboard.py
+++ soundboard.py
@@ -1,19 +1,24 @@
 import PySimpleGUI as sg
 import soundboardplay
 
+#row/column declaration
 MAX_ROWS = MAX_COL = soundboardplay.countSounds()
 
+#Button Layout for PysimpleGUI
 layout = [[sg.Button('+', size=(13, 10), key=(i,j), pad=(0,0)) for j in range(MAX_COL)] for i in range(MAX_ROWS)]
-
 window = sg.Window('Soundboard.tx', layout)
-soundboardplay.namesOfFiles()
-while True:                             # The Event Loop
+
+dicOfFiles = dict(soundboardplay.assignFiles(MAX_ROWS))
+while True:
+    # The Event Loop
     event, values = window.read()
     print(event, values)
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    if event == (1, 0):
-        soundboardplay.playSound('mission.wav')
+    if event in dicOfFiles.keys():
+        soundboardplay.playSound(dicOfFiles[event])
+    else:
+        print('no file assigned')
 window.close()
         
 
--- soundboardplay.py
+++ soundboardplay.py
@@ -1,7 +1,7 @@
 import os
 import pygame
 import glob
-
+#pygame initilization and setting levels/aliases
 pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512)
 txsound = 'tx.wav'
 channel0 = pygame.mixer.Channel(0)
@@ -11,23 +11,52 @@ channel1.set_volume(0.0,1.0)
 path = os.getcwd()
 
 def playSound(sound):
+    #plays sound on right ch and tx activation sound on left channel
     channel1.play(pygame.mixer.Sound(path +'/sounds/'+ sound))
-    channel0.play(pygame.mixer.Sound(path + '/sounds/' + txsound))
+    channel0.play(pygame.mixer.Sound(path + '/sounds/' + txsound),maxtime=20)
 
 def countSounds():
+    #Counts Files in /sounds/ folder. MP3 Play not implmented
     wav_counter = len(glob.glob1(path + '/sounds/', '*.wav')) - 1 #tx_tone
     mp3_counter = len(glob.glob1(path + '/sounds/', '*.mp3'))
     print(wav_counter)
     print (mp3_counter)
     return wav_counter
 
-def qtyRowCol(numofsounds):
-    return(wav_counter)
+
+def qtyRowCol():
+    # beginning of row and column calculation
+    return(countSounds())
+
 
 def namesOfFiles():
+    # Gets the names of all the mp3 and wav files and returns list
+    names = []
+    count = 0
     extensions = ('.mp3', '.wav')
     for subdir, dirs, files in os.walk(path+'/sounds/'):
         for file in files:
+            count += 1
             ext = os.path.splitext(file)[-1].lower()
             if ext in extensions:
-                print(os.path.join(file))
+                names.insert(count,os.path.join(file))
+    return(names)
+
+def makelistofpositions(numofrows):
+    ##Makes list of positions availible
+    positions = []
+    for row in range(numofrows):
+        for column in range(numofrows):
+            positions.append((row,column))
+    return positions
+
+def assignFiles(numofrows):
+    #assigns files to positons
+    file_positions = {}
+    names = namesOfFiles()
+    position_map = zip(makelistofpositions(numofrows),namesOfFiles())
+    return(position_map)
+
+
+
+
